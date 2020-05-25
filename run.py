acdcde167751232f846919b8f6a68f1801c3397c
wuyandong <wuyandong@douyu.tv>
2020-05-25 22:39:19 +0800

    123

--- run.py
+++ run.py
@@ -2,11 +2,12 @@
 from test_01 import *
 
 
-def test_add():
+def test_add(x, y):
     data = add(1, 2)
-    assert data == 4, "加法成立"
+    assert data == (x+y), "加法成立"
     print(data)
 
 
 if __name__ == '__main__':
-    test_add()
+    test_add(1, 2)
+    test_add(1, 3)
